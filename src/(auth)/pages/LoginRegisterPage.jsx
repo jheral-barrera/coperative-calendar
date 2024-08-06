42cdc3e7dde834cde63665d490505150c321c2aa
jheral-barrera <jheral.barrera@gmail.com>
2024-08-06 11:46:17 -0400

    [add] adding hook useForm to auth

--- src/(auth)/pages/LoginRegisterPage.jsx
+++ src/(auth)/pages/LoginRegisterPage.jsx
@@ -1,27 +1,62 @@
+import { useForm } from '../../hooks';
 import '../styles/loginRegisterPage.css';
 
+const loginFormFields = {
+	loginEmail: '',
+	loginPassword: '',
+}
+
+const registerFormFields = {
+	registerName: '',
+	registerEmail: '',
+	registerPassword: '',
+	registerPasswordConfirmation: '',
+}
+
 export const LoginRegisterPage = () => {
+
+	const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm( loginFormFields );
+	const { registerName, registerEmail, registerPassword, registerPasswordConfirmation, onInputChange: onRegisterInputChange } = useForm( registerFormFields );
+
+	const onLoginSubmit  = ( event ) => {
+		event.preventDefault();
+
+		console.log( { loginEmail, loginPassword } );
+	}
+
+	const onRegisterSubmit  = ( event ) => {
+		event.preventDefault();
+
+		console.log( { registerName, registerEmail, registerPassword, registerPasswordConfirmation } );
+	}
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={ onLoginSubmit }>
                         <div className="form-group mb-2">
                             <input 
+								name='loginEmail'
                                 type="text"
                                 className="form-control"
                                 placeholder="example@example.com"
+								onChange={ onLoginInputChange }
+								value={ loginEmail }
                             />
                         </div>
                         <div className="form-group mb-2">
                             <input
+								name='loginPassword'
                                 type="password"
                                 className="form-control"
                                 placeholder="Password"
+								onChange={ onLoginInputChange }
+								value={ loginPassword }
                             />
                         </div>
-                        <div className="form-group mb-2">
+                        <div className="form-group mb-2 text-center">
                             <input 
                                 type="submit"
                                 className="btnSubmit"
@@ -33,38 +68,50 @@ export const LoginRegisterPage = () => {
 
                 <div className="col-md-6 login-form-2">
                     <h3>Registro</h3>
-                    <form>
+                    <form onSubmit={ onRegisterSubmit }>
                         <div className="form-group mb-2">
                             <input
+								name='registerName'
                                 type="text"
                                 className="form-control"
                                 placeholder="Name"
+								onChange={ onRegisterInputChange }
+								value={ registerName }
                             />
                         </div>
                         <div className="form-group mb-2">
                             <input
+								name='registerEmail'
                                 type="email"
                                 className="form-control"
                                 placeholder="example@example.com"
+								onChange={ onRegisterInputChange }
+								value={ registerEmail }
                             />
                         </div>
                         <div className="form-group mb-2">
                             <input
+								name='registerPassword'
                                 type="password"
                                 className="form-control"
                                 placeholder="Password"
+								onChange={ onRegisterInputChange }
+								value={ registerPassword }
                             />
                         </div>
 
                         <div className="form-group mb-2">
                             <input
+								name='registerPasswordConfirmation'
                                 type="password"
                                 className="form-control"
                                 placeholder="Repeat password"
+								onChange={ onRegisterInputChange }
+								value={ registerPasswordConfirmation }
                             />
                         </div>
 
-                        <div className="form-group mb-2">
+                        <div className="form-group mb-2 text-center">
                             <input 
                                 type="submit" 
                                 className="btnSubmit" 
---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+	name: 'auth',
+	initialState: {
+		status: 'checking',
+		user: {},
+		errorMessage: undefined,
+	},
+	reducers: {
+		onCheking: ( state ) => {
+			state.status = 'checking';
+			state.user = {};
+			state.errorMessage = undefined;
+		},
+		onLogin: ( state, { payload } ) => { 
+			state.status = 'authenticated';
+			state.user = payload;
+			state.errorMessage = undefined;
+		},
+		onLogout: ( state ) => { 
+			state.isLoggedIn = 'no-authenticated';
+			state.user = {};
+			state.errorMessage = undefined;
+		},
+	},
+});
+
+export const { onLogin, onLogout } = authSlice.actions;
